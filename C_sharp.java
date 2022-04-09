dfea8ad5a3ee440b0a343c44bc3edea3ef07e2c9
Shubham Mahalle <shbhmmahalle3@gmail.com>
2022-04-09 08:27:15 +0530

    Automation code for test cases-101 is added

---
+++ C_sharp.java
@@ -0,0 +1,26 @@
+package Display;
+
+public class C_sharp 
+
+{
+	
+	//Given a string = "123AM256CD", 
+		//	print 2 output strings one for all the numbers and one for strings. 
+			//Provide 2 ways. (Asked for the exact code)
+
+	public static void main(String[] args) 
+	
+	{
+		
+		//String a = "123AM256CD";
+		
+		//System.out.println(a.replaceAll("[0-9]", ""));
+		//System.out.println(a.replaceAll("[^0-9]", ""));
+		
+		int a=2;
+		int b=1;
+		
+		System.out.println(Math.max(a, b));
+	}
+	
+}
